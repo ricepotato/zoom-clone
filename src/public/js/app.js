bf684d9e952714b73d4c04fc8ac853e883544dad
ricepotato <ricepotato40@gmail.com>
2021-08-30 23:40:55 +0900

    feat: room notification

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,11 +14,23 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(e) {
+  e.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(e) {
@@ -34,3 +46,7 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("Someone Joined!");
 });
+socket.on("bye", () => {
+  addMessage("Someone Left!");
+});
+socket.on("new_message", addMessage);
